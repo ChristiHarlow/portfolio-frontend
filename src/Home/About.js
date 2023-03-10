6d3b5519ad6cc6de3f2eed11fa3f87ebd860ee0d
Christi Harlow <cah120671@gmail.com>
2023-03-09 19:35:21 -0500

    Update

--- src/Home/About.js
+++ src/Home/About.js
@@ -7,22 +7,19 @@ const About = () => {
             <h3>About</h3>
             <p>-Full Stack Bootcamp Graduate</p>
             <p>
-                -Experienced Analyst with a demonstrated history of working in
-                the health care industry. Skilled in Team Supervision,
-                Healthcare and Health Insurance. Strong business development
-                professional with a Bachelor of Science (BS) focused in
-                Business, Management, and Economics.
+                Experienced Analyst working in Healthcare, Health Insurance, and
+                Team Supervision. Bachelor of Science (BS) focused in Business,
+                Management, and Economics.
             </p>
             <p>
-                -Team Supervisor lead a team of Contact Tracers working
-                remotely, that collectively performed the functions of the
-                Contact Tracing Initiative.
+                Team Supervisor lead a team of Contact Tracers working remotely,
+                that collectively performed the functions of the Contact Tracing
+                Initiative.
             </p>
             <p>
-                -Coding Analyst detected potential health care fraud, waste, and
-                abuse through the identification of aberrant coding and/or
-                billing patterns. Audits and analyzes claims for recovery
-                efforts.
+                Coding Analyst detected potential health care fraud, through the
+                identification of aberrant coding and/or billing patterns.
+                Audits and analyzes claims for recovery efforts.
             </p>
         </section>
     );
